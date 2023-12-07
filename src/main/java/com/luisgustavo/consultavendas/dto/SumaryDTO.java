2fa54f6b997ed280e3f2e13136a24bd937df2ad6
Luis Gustavo <gustavdev28@outlook.com>
2023-12-07 16:04:58 -0300

    consulta-vendas

---
+++ src/main/java/com/luisgustavo/consultavendas/dto/SumaryDTO.java
@@ -0,0 +1,39 @@
+package com.luisgustavo.consultavendas.dto;
+
+import com.luisgustavo.consultavendas.projection.SumaryProjection;
+
+public class SumaryDTO {
+ //Atributos
+    private  String sellerName;
+    private Double total;
+
+    //Construtores
+    public SumaryDTO() {
+
+    }
+
+    public SumaryDTO(String sellerName, Double total) {
+        this.sellerName = sellerName;
+        this.total = total;
+    }
+
+    public SumaryDTO(SumaryProjection x) {
+    }
+
+    //Getters e Setters
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+}
